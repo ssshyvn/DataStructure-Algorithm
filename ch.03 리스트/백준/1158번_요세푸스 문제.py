2f602278000ede540ce41ddc1f08cfd3f609546e
Seahyun Park <163717912+ssshyvn@users.noreply.github.com>
2024-10-07 14:45:19 +0900

    Update 1158번_요세푸스 문제.py

--- ch.03 리스트/백준/1158번_요세푸스 문제.py
+++ ch.03 리스트/백준/1158번_요세푸스 문제.py
@@ -1,13 +1,13 @@
 N, K=map(int, input().split())
 answer=[]
-arr=[i for i in range(1, N+1)]  # [1, 2, ..., N]
-num=0 # 제거 할 인덱스
+circle=[i for i in range(1, N+1)]  # [1, 2, ..., N] -> 원형 큐 사용
+num=0 # 몇번째 사람을 제거할건지
 
 while len(arr)!=0:
-    num+=K-1
+    num+=K-1    # 제거할 인덱스 설정(0부터 시작해야 함)
 
-    if num>=len(arr):
-        num=num%len(arr)
+    if num>=len(arr):       # 인덱스가 남아있는 배열 길이보다 클 경우
+        num=num%len(arr)    # 다시 맨 앞으로 돌아가야 함
     answer.append(str(arr.pop(num)))
 
 print("<", end="")

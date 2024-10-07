18b11b024f995358e5cca510f062d491b514f705
Seahyun Park <163717912+ssshyvn@users.noreply.github.com>
2024-10-07 13:36:01 +0900

    Update 1021번_회전하는 큐.py

--- ch.03 리스트/백준/1021번_회전하는 큐.py
+++ ch.03 리스트/백준/1021번_회전하는 큐.py
@@ -1,24 +1,25 @@
 from collections import deque
 
 n, m = map(int, input().split())
-p = list(map(int, input().split())) #뽑으려는 수위치
-dq = deque([i for i in range(1, n+1)])
+pick = list(map(int, input().split()))
+dq = deque([i for i in range(1, n+1)])      # 양쪽으로 삽입 삭제가 가능한 덱을 사용
 
 count = 0
 
-for i in p:
+for i in pick:
     while True:
-        if dq[0] == i:
-            dq.popleft()
+        if dq[0] == i:      # dq의 첫번째 원소가 현재 pick 원소와 같다면
+            dq.popleft()    # 제거 후 break
             break
-        else:
-            if dq.index(i) < len(dq)/2:
-                while dq[0] != i:
-                    dq.append(dq.popleft())
-                    count += 1
-            else:
-                while dq[0] != i:
-                    dq.appendleft(dq.pop())
-                    count +=1
+        else:                               # 같지 않다면 dq에서 현재 pick 원소를 찾아야 함.
+            if dq.index(i) < len(dq)/2:     # 현재 pick 원소가 dq의 앞쪽에 있으면 
+                while dq[0] != i:           # 앞 원소를 빼서 뒤로 보내는것이 빠르므로, idx가 dq의 맨 앞으로 올 때까지
+                    dq.append(dq.popleft()) # 2번 연산 수행 후
+                    count += 1              # count 하나 증가
+
+            else:                           # 현재 pick 원소가 dq의 뒤쪽에 있으면 
+                while dq[0] != i:           # 뒤 원소를 빼서 앞으로 보내는것이 빠르므로, idx가 dq의 맨 앞으로 올 때까지
+                    dq.appendleft(dq.pop()) # 3번 연산 수행 후
+                    count +=1               # count 하나 증가
 
 print(count)

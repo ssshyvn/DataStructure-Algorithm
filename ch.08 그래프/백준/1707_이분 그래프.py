b50df10d9d43ac99fb238e5c7f0ce51090670c1f
Seahyun Park <163717912+ssshyvn@users.noreply.github.com>
2024-11-23 23:21:55 +0900

    Create 1707_이분 그래프.py

---
+++ ch.08 그래프/백준/1707_이분 그래프.py
@@ -0,0 +1,33 @@
+import sys
+sys.setrecursionlimit(10**6)
+k = int(input())
+
+def DFS(start, visited, graph, group):
+  visited[start] = group  # 현재 노드의 그룹 저장
+
+  # 인접 노드 탐색
+  for v in graph[start]:
+    if visited[v] == 0:  # 아직 방문하지 않은 노드
+      result = DFS(v, visited, graph, -group) # -group : 현재 노드의 그룹과 다른 값 전달
+      if not result:
+        return False
+    else:
+      if visited[v] == group:  # 이미 방문한 곳 중에서 노드가 현재 그룹과 같으면 이분 그래프가 아님
+        return False
+  return True
+
+for i in range(k):
+    V, E = map(int, sys.stdin.readline().split())
+    graph = [[] for i in range(V+1)]
+    visited = [0] * (V+1)
+    for i in range(E):
+        a, b = map(int, sys.stdin.readline().split())
+        graph[a].append(b)
+        graph[b].append(a)
+
+    for j in range(1, V+1):
+        if visited[j] == 0:
+            result = (DFS(j, visited, graph, 1))
+            if not result:
+                break
+    print("YES") if result else print("NO")

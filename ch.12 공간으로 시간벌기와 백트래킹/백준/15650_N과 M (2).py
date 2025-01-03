6668e3c9b3475b9e37ac8494ddb85a63ee80634a
Seahyun Park <163717912+ssshyvn@users.noreply.github.com>
2025-01-03 19:57:55 +0900

    Create 15650_N과 M (2).py

---
+++ ch.12 공간으로 시간벌기와 백트래킹/백준/15650_N과 M (2).py
@@ -0,0 +1,18 @@
+def solve():
+    n, m = map(int, input().split())
+    
+    s = []
+    
+    def backtrack(k, start):
+        if k == m:
+            print(*s)
+            return
+        
+        for i in range(start, n + 1):
+            s.append(i)
+            backtrack(k + 1, i + 1)
+            s.pop()
+                
+    backtrack(0, 1)
+
+solve()
